2dc47c9268a5dc6b813998752e6be7352d6634a1
JaeHun Jung <jjh44723@gmail.com>
2025-04-27 15:15:46 -0500

    Unnecessary/Deprecated keymaps removed

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -11,29 +11,19 @@ vim.keymap.set('n', '<leader>d', '<Cmd>BufferDelete<CR>')
 -- Pin/unpin buffer
 vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>')
 
--- Sort automatically by...
-vim.keymap.set('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
-vim.keymap.set('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>')
-vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>')
-vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>')
-vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
-
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
 
 -- NOTE: Git plugins related keymaps
-vim.keymap.set('n', '<leader>gn', '<cmd>Gitsigns next_hunk<CR>', { desc = 'Go to next git hunk' })
-vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns prev_hunk<CR>', { desc = 'Go to prev git hunk' })
-vim.keymap.set('n', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>', { desc = 'Reset current hunk on cursor' })
+vim.keymap.set('n', '<leader>hn', '<cmd>Gitsigns next_hunk<CR>', { desc = 'Go to next git hunk' })
+vim.keymap.set('n', '<leader>hp', '<cmd>Gitsigns prev_hunk<CR>', { desc = 'Go to prev git hunk' })
+vim.keymap.set('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>', { desc = 'Reset current hunk on cursor' })
 vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>')  -- Git diff
 vim.keymap.set('n', '<leader>gc', ':G commit<CR>')     -- Git commit
 vim.keymap.set('n', '<leader>gp', ':G push<CR>')       -- Git push
 vim.keymap.set('n', '<leader>gl', ':G pull<CR>')       -- Git pull
 vim.keymap.set('n', '<leader>gf', ':G fetch<CR>')      -- Git fetch
-
--- NOTE: Neogit keymaps
-vim.keymap.set('n', '<leader>ng', '<cmd>Neogit<CR>', { desc = 'Open neogit' })
 
 -- NOTE: Copy line without \n with key 'Y'
 vim.keymap.set('n', 'Y', '^y$', { desc = 'Copy whole line without \n' })
@@ -42,10 +32,6 @@ vim.keymap.set('n', 'Y', '^y$', { desc = 'Copy whole line without \n' })
 vim.keymap.set('n', 'gv', '<cmd> vsplit | lua vim.lsp.buf.definition()<CR>', { desc = 'Open LSP definition on vsplit' })
 vim.keymap.set('n', 'gs', '<cmd> belowright split | lua vim.lsp.buf.definition()<CR>',
     { desc = 'Open LSP definition on split' })
-
--- NOTE: Portal keymap for the jumplist
-vim.keymap.set('n', '<leader>o', '<cmd>Portal jumplist backward<cr>', { desc = 'Portal jumplist backward' })
-vim.keymap.set('n', '<leader>i', '<cmd>Portal jumplist forward<cr>', { desc = 'Portal jumplist forward' })
 
 -- NOTE: abbreviate "vert sb" into vsb
 vim.cmd 'cnorea vsb vert sb'
@@ -68,16 +54,9 @@ vim.keymap.set('n', '<C-p>', '<cmd>cprev<CR>', { desc = 'go to prev quickfix ite
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
---
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- NOTE: Copy current buffer filename
 vim.keymap.set('n', '<leader>yf', '<cmd>let @+ = expand("%:t")<CR>',
@@ -96,7 +75,7 @@ vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>', { desc = 'Select
 -- NOTE: Commands
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save buffer' })
 -- NOTE: ToggleTerm shortcut
-vim.keymap.set({ 'n', 't' }, '<leader>t', '<cmd>ToggleTerm direction=horizontal size=25<cr>',
+vim.keymap.set({ 'n', 't' }, '<leader>t', '<cmd>ToggleTerm direction=horizontal size=15<cr>',
     { desc = 'Toggleterm on vertical' })
 
 -- NOTE: scroll left and right with 20 chars
