5f8998e432780561da8fe84acc9612206a768de8
JaeHun0304 <jjh44723@gmail.com>
2025-08-11 20:35:00 -0500

    remove go-to-preview

--- lua/custom/plugins/goto.lua
+++ lua/custom/plugins/goto.lua
@@ -1,12 +1,5 @@
 -- ~/.config/nvim/lua/custom/plugins/goto.lua
 return {
-  -- NOTE: Plugins to preview LSP definition
-  {
-    'rmagatti/goto-preview',
-    opts = {
-      default_mappings = true,
-    },
-  },
   -- NOTE: open specific line of file
   { 'wsdjeg/vim-fetch' },
 }
